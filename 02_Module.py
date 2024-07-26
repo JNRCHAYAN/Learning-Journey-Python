eb5072868b45b626f6c6351f76b567c079e0c403
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-27 00:32:30 +0600

    Use pyjokes module

---
+++ 02_Module.py
@@ -0,0 +1,6 @@
+# In Python, Modules are simply files with the “. py” extension containing Python code that can be imported inside another Python Modules Operations Program
+
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
