3fad60e5c25b071713739bdb0f80764e87209183
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-27 09:32:36 +0600

    more operator

--- 03_Variable_Datatype.py
+++ 03_Variable_Datatype.py
@@ -1,5 +1,30 @@
 a=1
 b=3
 st = "Chayan"
+c= True #c is boolean variable
+e = None #e is a none type variable
+b-=4
+a*=3
 print(a+b)
+print(a>b)
 print(st)
+print(c)
+print(e)
+
+# comparison operators 
+print("comparison operators ")
+ee = 5==6
+print(ee)
+print(a>b)
+print(a>=5)
+
+# Logical operators 
+print("Logical operators ")
+e = True or False
+print(e)
+r=10
+t=20
+test = r or t
+print(test)
+test = r and t
+print(test)
