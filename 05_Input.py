0fc2168ee3bcb07f17a4b1efb16cec0722a2b5c1
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-27 09:45:58 +0600

    Take user input

--- 05_Input.py
+++ 05_Input.py
@@ -3,3 +3,13 @@ b = input("Enter Number 2 :")
 
 print("Number a is :",a)
 print("Number b is :",b)
+print ("Add two string : ",a+b)
+
+
+a = int(input("Enter Number 1 :"))
+b = int(input("Enter Number 2 :"))
+
+
+print("Number a is :",a)
+print("Number b is :",b)
+print ("Sum of two number : ",a+b)
