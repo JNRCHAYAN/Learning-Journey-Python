3fab05afd6d77992543a1f50d3dd0154dd193a70
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-29 21:58:42 +0600

    List and tuples

---
+++ Practice Problem/problme5.py
@@ -0,0 +1,16 @@
+number =[]
+f1 = input("Enter Your number 1 : ")
+number.append(f1)
+f2 = input("Enter Your number 2 : ")
+number.append(f2)
+f3 = input("Enter Your number 3 : ")
+number.append(f3)
+f4 = input("Enter Your number 4 : ")
+number.append(f4)
+f5 = input("Enter Your number 5 : ")
+number.append(f5)
+
+print(number)
+number.sort()
+print(number)
+
