4688d1e35cb356b353b8e56bfb85a5db5617d65a
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-29 21:47:58 +0600

    Tuples Method

---
+++ 12_Tuples_Methods.py
@@ -0,0 +1,16 @@
+a=("Apple" ,59, "Orange", False,59, "Chayan")
+print(a)
+
+no = a.count(59)
+print(no)
+
+i = a.index("Apple")
+print(i)
+
+repated = a*2
+print(repated)
+
+print(2 in a)
+print(59 in a)
+
+print(len(a))
