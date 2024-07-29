80c48d6a42c98842f274790ef162c13b9f79e563
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-29 21:35:30 +0600

    Use many list method

---
+++ 10_List_methods.py
@@ -0,0 +1,18 @@
+friends = ["Apple" , "Orange", False,59, "Chayan"]
+print(friends)
+
+friends.append("Korim") # add new value in the end position
+print(friends)
+
+l1 = [1,6,444,75,547,4,3,8]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3,12334)
+print(l1)
+
+print(l1.pop(3))  # remove enter index value and also return this value 
+print(l1)
