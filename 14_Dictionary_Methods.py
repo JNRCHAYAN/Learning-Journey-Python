69ea840e0ef43db2b9d02de033159b2a09926be9
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-30 13:28:09 +0600

    Dictionary Methods

--- 14_Dictionary_Methods.py
+++ 14_Dictionary_Methods.py
@@ -10,3 +10,10 @@ print(marks.items())
 print(marks.keys())
 
 print(marks.values())
+
+marks.update({"Chayan":99 ,"Prio":100})  # It is mutable that is why it's change
+print(marks.items())
+
+print(marks.get("JNR"))
+# print(marks["jnr"])   This is show error that is why we can use get function
+print(marks["JNR"])
