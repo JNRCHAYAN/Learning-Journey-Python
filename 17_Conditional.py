1ec953b7f41498396d32309120d47f2188fe61bf
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-31 22:50:08 +0600

    17_Conditional

---
+++ 17_Conditional.py
@@ -0,0 +1,14 @@
+a = int(input("Enter Number 1 : "))
+b = int(input("Enter Number 2 : "))
+
+# if elif else ladder 
+
+if(a==b):
+    print("This number is Equal")
+
+elif(a<0):
+    print("Your enter wrong value")
+
+
+else:
+    print("No Equal")
