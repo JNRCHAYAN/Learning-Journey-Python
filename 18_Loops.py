60295a4f95cdcfa46904be02f3aef22b93ed3c61
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-31 23:22:30 +0600

    Loops

---
+++ 18_Loops.py
@@ -0,0 +1,28 @@
+#  While Loop ands for loop 
+
+i =0
+while(i<=10):
+    print(i)
+    i+=1
+
+# list using while loop 
+
+l = [1,3,5,"chayan", False,"This" , "Roni"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
+
+# For Loop 
+print("\nFor Loop")
+for i in range(4):
+    print(i)
+print("\nFor Loop Rnage")
+for i in range(0,10,4):
+    print(i)
+
+print("\nFor Loop in list")
+
+l = [1,4,6,7,3,6,"Chayan",4]
+for i in l:
+    print(i)
