fc83d4315a54cb24c81fb5ae49cee3f98fac165f
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-01 10:01:53 +0600

    Break Contiune and pass in python

---
+++ 19_Break_Continue_Pass.py
@@ -0,0 +1,16 @@
+for i in range (100):
+    if(i==34):
+        break  # Exit in the loop right now
+    print(i)
+
+for i in range (100):
+    if(i==34):
+        continue  # Sikp value in the loop
+    print(i)
+
+for i in range (100):
+     pass # Fature Work  Pas is a null statement
+i=0
+while(i<45):
+       print(i)
+       i+=1
