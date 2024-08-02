e63c6f67ffb89ad82205fa08c2acd723fd118b3a
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-02 23:28:35 +0600

    Function Call

---
+++ 20_Function.py
@@ -0,0 +1,10 @@
+def avg():
+    a= int(input("Enter Your Number : "))
+    b= int(input("Enter Your Number : "))
+    c= int(input("Enter Your Number : "))
+    avag = (a+b+c)/3
+    print(avag)
+
+avg()
+avg()
+avg()
