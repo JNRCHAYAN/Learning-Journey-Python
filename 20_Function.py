9b774540f557c6f86a770c8d6ac63bf0c3f52301
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-02 23:37:07 +0600

    Function with argument

--- 20_Function.py
+++ 20_Function.py
@@ -1,3 +1,4 @@
+# Function Defination
 def avg():
     a= int(input("Enter Your Number : "))
     b= int(input("Enter Your Number : "))
@@ -5,6 +6,15 @@ def avg():
     avag = (a+b+c)/3
     print(avag)
 
+#Function Call
 avg()
-avg()
-avg()
+
+
+#Function with argument
+
+def goodday(name, ending):
+    print("Good Day "+name)
+    print(ending)
+
+goodday("Chayan" ,"Thank You")
+goodday("Roni","Thanks")
