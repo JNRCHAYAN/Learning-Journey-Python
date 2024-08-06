d1ac2a195d0744d935e16c42dbb317fad9fe468f
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 07:31:01 +0600

    Instance attribute

--- 25_Class_object.py
+++ 25_Class_object.py
@@ -1,7 +1,15 @@
 class Employee:
-    name = "Harry"
-    language = "py"
+    language = "py"  # This is an class attribute
     salary = 12000
 
+    def getinfo(self):  # Must be use an arrgumet other wise we can see some error
+        print(f"The language is {self.language}. The salary is {self.salary}")
+    def greet(self):
+        print("Good Morning")
+
 chayan = Employee()
+chayan.name = "Harry" # This is instance attribute
 print(chayan.name ,chayan.language , chayan.salary)
+
+chayan.getinfo()  
+chayan.greet()
