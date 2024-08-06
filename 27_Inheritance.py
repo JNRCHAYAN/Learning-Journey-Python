9bae314bdd9811e4c57c267fff6f4d0e5091d5cd
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-07 00:48:16 +0600

    Inheritance

--- 27_Inheritance.py
+++ 27_Inheritance.py
@@ -5,10 +5,13 @@ class employee:
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
 
-class codder:
+class codder(employee):
     programming = "Python"
+    def __init__(self) -> None:
+        super().__init__() # For Super class constructor Function Access
+        print("Codder Constructor Function")
 
-class programmer(employee,codder):
+class programmer(codder):
     companyw = "IIC Company"
     def showLanguage(self):
         print(f"The name is {self.name} and the salary is {self.language}")
