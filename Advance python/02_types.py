afa24ba8fe8ad75bbf27580d1e8fc0b9370f3ed0
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-08 17:45:38 +0600

    types in python

---
+++ Advance python/02_types.py
@@ -0,0 +1,12 @@
+from typing import List,Union,Tuple,Dict
+
+numbers: List[int] = {1,4,6,3,4}
+person: Tuple[int ,str] = {"Roy",2}
+
+n : int =5
+name : str = "Chayan"
+
+def sum(a: int ,b:int)->int:
+    return a+b
+
+sum(5,6)
