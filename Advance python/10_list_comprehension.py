a1bfee294e1a45f19d24532edcd3fbbc201c5b01
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-08 21:28:20 +0600

    10_list_comprehension

---
+++ Advance python/10_list_comprehension.py
@@ -0,0 +1,12 @@
+mylist = [1,4,5,3,5,3]
+
+sqlist =[]
+for item in mylist:
+    sqlist.append(item*item)
+
+print(sqlist)
+
+# Easy way to do this
+
+newsqlist = [i*i for i in mylist]
+print(newsqlist) 
