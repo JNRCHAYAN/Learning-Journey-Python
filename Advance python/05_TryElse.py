a4290b42348d61a44e7cc6a86b9d8c1db1d8c11c
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-08 18:00:35 +0600

    05_TryElse

---
+++ Advance python/05_TryElse.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("Enter a number : "))
+    print(a)
+
+
+except Exception as e:
+    print(e)
+    
+# If there is no error then it show 
+else:
+    print("Inside Else")  
+
