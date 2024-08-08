ada40219dd79cc1f1953f19dad2f65f3c81a0045
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-08 21:16:24 +0600

    Module

---
+++ Advance python/Module.py
@@ -0,0 +1,9 @@
+def myFunc():
+    print("Chayan Roy")
+
+myFunc()
+print(__name__)    #Show file Name which file is run
+
+if __name__ == "__main__":
+    #if this code is excuted by running this present in
+    print("We are directly runing this code")
